d753cc5ae573769238ebbbfce97527fe9b32eda9
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-09 17:18:03 +0000

    added route for just /python url

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c(text):
     return "C {}".format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))

3d6f1d167e485eb90db3d47941dc1b26f161c2ca
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-09 18:50:14 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -30,6 +31,11 @@ def python(text="is cool"):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

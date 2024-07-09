3606dccf8240ef0c70acf9be394becf13cdd90a3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-09 16:32:52 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from flask import Flask
+"""A script that starts a Flask web application"""
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays message"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

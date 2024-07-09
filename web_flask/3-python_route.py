d17cc9b13e927aaa6acf13118a9826595d9ccaf5
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-09 16:44:42 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask
+"""A script that starts a Flask web application"""
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays message"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
